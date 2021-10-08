6bbdee272fb42a4eb5676183f2dec1ea2b3a370c
Fraser Wright <fraser.wright@eqtr.com>
2021-10-08 11:56:22 +0100

    Update ScreenMediaTtContext.cs

--- src/ScreenMediaTT/ScreenMediaTT.Data/ScreenMediaTtContext.cs
+++ src/ScreenMediaTT/ScreenMediaTT.Data/ScreenMediaTtContext.cs
@@ -17,21 +17,7 @@ namespace ScreenMediaTT.Data
 
         public string DbPath { get; private set; }
 
-        //public ScreenMediaTtContext()
-        //{
-        //    var folder = Environment.SpecialFolder.LocalApplicationData;
-        //    var path = Environment.GetFolderPath(folder);
-        //    DbPath = $"{path}{System.IO.Path.DirectorySeparatorChar}app.db";
-        //}
-
         public ScreenMediaTtContext(DbContextOptions options) : base(options) { }
-
-
-        //// The following configures EF to create a Sqlite database file in the
-        //// special "local" folder for your platform.
-        //protected override void OnConfiguring(DbContextOptionsBuilder options)
-        //    => options.UseSqlite($"Data Source={DbPath}");
-
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
